256e8f6f5aed422073454ea90e69be8338d46739
jansi <jan.siebrands@web.de>
2022-06-20 20:12:30 +0200

    Finished 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductListComponent} from "./product-list/product-list.component";
+import {FormsModule} from "@angular/forms";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
